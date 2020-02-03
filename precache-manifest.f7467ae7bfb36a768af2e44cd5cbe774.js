57e6c5c1b14e8b3b3f698abe7e245915c187ffe7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-03 22:24:31 +0000

    Deploy makstep/resume to github.com/makstep/resume.git:gh-pages

---
+++ precache-manifest.f7467ae7bfb36a768af2e44cd5cbe774.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0506dfe5479cd727b9084a7025477957",
+    "url": "/resume/static/media/portaint.0506dfe5.jpg"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "d64eda0e375871fdbaae",
+    "url": "/resume/static/js/main.56edd4c1.chunk.js"
+  },
+  {
+    "revision": "7f8f1280ce0edaf392f2",
+    "url": "/resume/static/js/2.0c2ccc16.chunk.js"
+  },
+  {
+    "revision": "d64eda0e375871fdbaae",
+    "url": "/resume/static/css/main.6d6da32e.chunk.css"
+  },
+  {
+    "revision": "7f8f1280ce0edaf392f2",
+    "url": "/resume/static/css/2.2811987d.chunk.css"
+  },
+  {
+    "revision": "cdee20a5ba52905393de014e35f9c69b",
+    "url": "/resume/index.html"
+  }
+];
